45d977cd315dae0c0f2f7992a8a2b83f3c8bc109
FLAVIO JOSE DOS SANTOS <santos.flavio@edu.fecap.br>
2025-01-31 11:16:33 -0300

    [ADD]Tela principal, menu lateral e inicialização pela tela de Login

--- app/src/main/java/br/maratona/fecap/calculadora_khipo/Login.java
+++ app/src/main/java/br/maratona/fecap/calculadora_khipo/Login.java
@@ -10,6 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class Login extends AppCompatActivity {
 
     Button btnEntrar;
